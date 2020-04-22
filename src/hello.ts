7a132291d04240d4a57b89f1b08cc3b6bbbf8a3a
KatagiriSo <>
2020-04-22 19:03:43 +0900

    test code

--- src/hello.ts
+++ src/hello.ts
@@ -1,4 +1,11 @@
 import { utilFunc } from './util';
+import * as fs from 'fs';
+import * as path from 'path';
+
 
 console.log("Hello, typescript!");
 console.log(utilFunc("test"));
+console.log(process.cwd());
+const filePath:string = path.join(process.cwd(), "dist/util.js");
+const text = fs.readFileSync(`${filePath}`, {encoding:"utf-8"});
+console.log(text);
